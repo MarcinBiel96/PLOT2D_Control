32acd25c166faecb7ff4f933ef14e62027d9ea1b
mb960 <mb960@KOMPUTER90>
2023-03-12 08:56:41 +0100

    Dodaj pliki projektów.

---
+++ UserControl1.Designer.vb
@@ -0,0 +1,39 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserControl1
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 przesłania metodę dispose, aby wyczyścić listę składników.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wymagane przez Projektanta formularzy systemu Windows
+    Private components As System.ComponentModel.IContainer
+
+    'UWAGA: następująca procedura jest wymagana przez Projektanta formularzy systemu Windows
+    'Możesz to modyfikować, używając Projektanta formularzy systemu Windows. 
+    'Nie należy modyfikować za pomocą edytora kodu.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'UserControl1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.DoubleBuffered = True
+        Me.Name = "UserControl1"
+        Me.Size = New System.Drawing.Size(511, 511)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+End Class
---
+++ UserControl1.vb
@@ -0,0 +1,32 @@
+﻿Public Class UserControl1
+    Dim plot As Bitmap
+    Dim gfx As Graphics
+    Dim dataset() As Double
+    Dim pointset() As PointF
+    Dim zoomY As Double = -50
+    Dim dispY As Double = 0
+    Dim zoomX As Double = 1
+    Dim dispX As Double = 0
+
+    Private Sub UserControl1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ''''''''''''''''''''''
+        Dim data(1000) As Double
+        ReDim dataset(1000)
+        For i = 0 To 1000
+            dataset(i) = Math.Sin(i / 10)
+        Next
+        ''''''''''''''''''''''
+        plot = New Bitmap(Width, Height)
+        ReDim pointset(dataset.Length - 1)
+        For i = 0 To dataset.Length - 1
+            pointset(i) = New PointF(i * zoomX - dispX, dataset(i) * zoomY + Height \ 2 + 1 - dispY)
+        Next
+        gfx = Graphics.FromImage(plot)
+        gfx.DrawLine(Pens.DimGray, 0, Height \ 2 + 1 - CType(dispY, Integer), Width, Height \ 2 + 1 - CType(dispY, Integer)) 'oś X
+        gfx.DrawLines(Pens.Lime, pointset) 'wykres
+    End Sub
+
+    Private Sub UserControl1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        e.Graphics.DrawImage(plot, 0, 0)
+    End Sub
+End Class
